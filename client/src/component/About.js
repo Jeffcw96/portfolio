ec2d0425c50e27f0f960669fda2cf82ac079bdc1
JeffCW <changcheewah96@gmail.com>
2021-03-16 22:28:24 +0800

    text shadow effect in About Page

--- client/src/component/About.js
+++ client/src/component/About.js
@@ -6,19 +6,25 @@ export default function About({ aboutRef }) {
     return (
         <div className="about-page flex align-center" ref={aboutRef}>
             <div className="typewriting-text flex space-between align-center">
-                <Typewriter
-                    onInit={(typewriter) => {
-                        typewriter
-                            .typeString("Hi, My name is Jeff Chang<br/>I'm working as a software engineer<br/><br/>")
-                            .pauseFor(1000)
-                            .typeString("I'm always looking for new technology stack and trying<br/>them out in a project.<br/>")
-                            .typeString("Do checkout some projects I've done during my free time below!")
-                            .start();
-                    }}
-                    options={{
-                        delay: 50
-                    }}
-                />
+                <div class="type-writer-container">
+                    <em className="type-writer-body-tag open"> &lt;body&gt;</em>
+                    <em className="type-writer-body-tag closed"> &lt;/body &gt;</em>
+                    <em className="type-writer-html-tag open"> &lt;html&gt;</em>
+                    <em className="type-writer-html-tag closed"> &lt;/html &gt;</em>
+                    <Typewriter
+                        onInit={(typewriter) => {
+                            typewriter
+                                .typeString("Hi, My name is Jeff Chang<br/>I'm working as a software engineer<br/><br/>")
+                                .pauseFor(1000)
+                                .typeString("I'm always looking for new technology stack and trying<br/>them out in a project.<br/>")
+                                .typeString("Do checkout some projects I've done during my free time below!")
+                                .start();
+                        }}
+                        options={{
+                            delay: 50
+                        }}
+                    />
+                </div>
                 <div>
                     <img src={profilePic} className="profile-pic" alt="profile picture" />
                 </div>
