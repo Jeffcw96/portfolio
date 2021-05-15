aad4ce61bb21073a97600effff1f34bfe915b8ee
JeffCW <changcheewah96@gmail.com>
2021-05-15 20:43:48 +0800

    done modal ui ux, ready for clean up and deployment

--- client/src/component/ProjectDetailsModal.js
+++ client/src/component/ProjectDetailsModal.js
@@ -22,7 +22,6 @@ export default function ProjectDetailsModal({ active, closeModal }) {
             )
         }
     }
-
     return (
         <div id="myModal" className={`modal ${active ? 'active' : ''}   `}>
             <div className="modal-content">
@@ -39,14 +38,9 @@ export default function ProjectDetailsModal({ active, closeModal }) {
                                     tag="section"
                                     wrapperTag="ul"
                                     navigation
-                                    pagination
+
                                     spaceBetween={0}
                                     slidesPerView={1}
-                                    onInit={(swiper) => console.log('Swiper initialized!', swiper)}
-                                    onSlideChange={(swiper) => {
-                                        console.log('Slide index changed to: ', swiper.activeIndex);
-                                    }}
-                                    onReachEnd={() => console.log('Swiper end reached')}
                                 >
                                     {slides}
                                 </Swiper>
@@ -59,17 +53,32 @@ export default function ProjectDetailsModal({ active, closeModal }) {
                                     <div className="project-detail-component">
                                         <span>{projectDetailsInfo.descriptions}</span>
                                     </div>
-                                    <div className="project-detail-component" ><span>Tags :</span>
+                                    <div className="project-detail-component" ><span style={{ marginRight: '10px' }}>Tags: </span>
                                         <div className="project-tag-container">
-                                            {projectDetailsInfo.tags.map(tagInfo => (
-                                                <span className="project-detail-tag" style={{ backgroundColor: tagInfo.backgroundColor, color: tagInfo.color }}>{tagInfo.tag}</span>
+                                            {projectDetailsInfo.tags.map((tagInfo, ind) => (
+                                                <span key={ind} className="project-detail-tag" style={{ backgroundColor: tagInfo.backgroundColor, color: tagInfo.color }}>{tagInfo.tag}</span>
                                             ))}
                                         </div>
                                     </div>
-                                    <div class="call-to-action-container">
-                                        <div class="call-to-action">
-                                            <a href={projectDetailsInfo.slug} target="_blank"><div>View Project</div></a>
-                                            <a href={projectDetailsInfo.source} target="_blank"><div>Source Code</div></a>
+                                    <div className="call-to-action-container">
+                                        <div className="call-to-action">
+                                            <a href={projectDetailsInfo.slug} target="_blank">
+                                                <div className="flex align-center">
+                                                    <span className="view-live-icon">
+                                                        <svg viewBox="0 0 500 500">
+                                                            <path d="M 60,65 A 50,50 0 0 1 135.14,21.97 L 390.45,211.91 A 50,50 0 0 1 390.45,288.09 L 135.14,478.03 A 50,50 0 0 1 60,435 Z" />
+                                                        </svg>
+                                                    </span>
+                                                    <span className="call-to-action-text">
+                                                        View Project
+                                                    </span>
+                                                </div></a>
+                                            <a href={projectDetailsInfo.source} target="_blank">
+                                                <div className="flex align-center">
+
+                                                    <span className="call-to-action-text">
+                                                        Source Code
+                                                    </span></div></a>
                                         </div>
                                         <div className="close" onClick={closeModal}>&times;</div>
                                     </div>
